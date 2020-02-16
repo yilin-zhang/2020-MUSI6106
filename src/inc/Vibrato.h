a5064ade470443fd8abbf4d1779b023d9094ba81
Yilin Zhang <yzhang0129@outlook.com>
2020-02-16 15:47:39 -0500

    Reject negative block size.

--- src/inc/Vibrato.h
+++ src/inc/Vibrato.h
@@ -63,6 +63,8 @@ class CVibrato {
             err = m_Lfo.setParam(CLfo::kParamFreq, fParam);
             break;
         case kParamBlockSize:
+            if (fParam <= 0)
+                return kFunctionInvalidArgsError;
             m_iBlockSize = int(fParam);
         default:
             return kFunctionIllegalCallError;
